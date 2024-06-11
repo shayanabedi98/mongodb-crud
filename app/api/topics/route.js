837d94748be634a1edff253360fa19492dafb75d
shayanabedi98 <shayanabedi@hotmail.com>
2024-06-10 22:45:03 -0400

    ...

--- app/api/topics/route.js
+++ app/api/topics/route.js
@@ -2,10 +2,9 @@ import connectMongoDB from "@/libs/mongodb";
 import Topic from "@/models/topic";
 import { NextResponse } from "next/server";
 
-export async function POST(req) {
-  const { title, description } = await req.json();
+export async function POST(request) {
+  const { title, description } = await request.json();
   await connectMongoDB();
-
   await Topic.create({ title, description });
   return NextResponse.json({ message: "Topic Created" }, { status: 201 });
 }
@@ -16,9 +15,9 @@ export async function GET() {
   return NextResponse.json({ topics });
 }
 
-export async function DELETE(req) {
-  const id = req.nextUrl.searchParams.get("id");
+export async function DELETE(request) {
+  const id = request.nextUrl.searchParams.get("id");
   await connectMongoDB();
   await Topic.findByIdAndDelete(id);
-  return NextResponse.json({ message: "Topic Deleted" }, { status: 200 });
+  return NextResponse.json({ message: "Topic deleted" }, { status: 200 });
 }
--- components/TopicsList.jsx
+++ components/TopicsList.jsx
@@ -3,18 +3,19 @@ import RemoveBtn from "./RemoveBtn";
 import { HiPencilAlt } from "react-icons/hi";
 
 const getTopics = async () => {
-  const apiUrl = process.env.API_URL;
+  const apiUrl = process.env.API_URL
   try {
     const res = await fetch(`${apiUrl}/api/topics`, {
-      cache: "no-cache",
+      cache: "no-store",
     });
 
     if (!res.ok) {
-      throw new Error("failed to fetch topics");
+      throw new Error("Failed to fetch topics");
     }
+
     return res.json();
   } catch (error) {
-    console.log(error);
+    console.log("Error loading topics: ", error);
   }
 };
 
@@ -23,15 +24,16 @@ export default async function TopicsList() {
 
   return (
     <>
-      {topics.map((t, index) => (
+      {topics.map((t) => (
         <div
-          key={index}
+          key={t._id}
           className="p-4 border border-slate-300 my-3 flex justify-between gap-5 items-start"
         >
           <div>
             <h2 className="font-bold text-2xl">{t.title}</h2>
             <div>{t.description}</div>
           </div>
+
           <div className="flex gap-2">
             <RemoveBtn id={t._id} />
             <Link href={`/editTopic/${t._id}`}>
